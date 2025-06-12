bfdd92b8f890c6732b551904b3df6f99322f226d
19deepak98 <deepakkumar21158@gmail.com>
2025-06-12 17:35:58 +0530

    removed token

--- flask-app/app.py
+++ flask-app/app.py
@@ -9,5 +9,4 @@ def home():
 if __name__ == "__main__":
     API_KEY = "sk_test_51FakeKeyFodsrGitleaksDetection"
     passwd = "password123"
-    token = "hkh89dsbb"
     app.run(host="0.0.0.0", port=5000)
