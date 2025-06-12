552d61c21309ee2d958359b465f22123bbce8ac6
19deepak98 <deepakkumar21158@gmail.com>
2025-06-12 17:22:58 +0530

    added random token field

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,4 +7,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    API_KEY = "sk_test_51FakeKeyForGitleaksDetection"
+    passwd = "password123"
     app.run(host="0.0.0.0", port=5000)
