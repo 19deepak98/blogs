6473f8db6caa38d3f0c215eb15063324171d46ea
19deepak98 <deepakkumar21158@gmail.com>
2025-06-12 17:25:36 +0530

    new token

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,6 +7,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    API_KEY = "sk_test_51FakeKeyForGitleaksDetection"
+    API_KEY = "sk_test_51FakeKeyFodsrGitleaksDetection"
     passwd = "password123"
+    token = "hkh89dsbb"
     app.run(host="0.0.0.0", port=5000)
